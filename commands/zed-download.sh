1de597d9548f177539831f6fb8f9ffe0d613e406
jamoowen <james.owen@superpayments.com>
2025-10-04 12:07:33 +0100

    added zed download upload script

---
+++ commands/zed-download.sh
@@ -0,0 +1,3 @@
+cp ~/.config/nvim/zed/keymap.json ~/.config/zed/keymap.json
+cp ~/.config/nvim/zed/settings.json ~/.config/zed/settings.json
+cp ~/.config/nvim/zed/tasks.json ~/.config/zed/tasks.json
---
+++ commands/zed-upload.sh
@@ -0,0 +1,3 @@
+cp  ~/.config/zed/keymap.json ~/.config/nvim/zed/keymap.json
+cp  ~/.config/zed/settings.json ~/.config/nvim/zed/settings.json
+cp  ~/.config/zed/tasks.json ~/.config/nvim/zed/tasks.json
